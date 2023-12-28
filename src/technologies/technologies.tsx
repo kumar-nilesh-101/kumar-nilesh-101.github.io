eb074fb9d7af34908177de6976176335a86eb1de
nilesh kumar <nilesh22.a67@gmail.com>
2023-12-28 19:56:05 +0530

    repositioning tooltip

--- src/technologies/technologies.tsx
+++ src/technologies/technologies.tsx
@@ -8,8 +8,8 @@ export function Technologies() {
             {
                 tech.map(t => {
                     return (
-                        <div title={t.name} className='p-xxl-5 col-3 d-flex align-items-center justify-content-around'>
-                            <div className="thumbnail">
+                        <div className='p-xxl-5 col-3 d-flex align-items-center justify-content-around'>
+                            <div title={t.name} className="thumbnail">
                                 <img src={t.thumbUrl} alt={t.name} />
                             </div>
                         </div>
